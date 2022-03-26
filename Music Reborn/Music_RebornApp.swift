7dc17fad8a2df73020789151caa90206ce382c72
llsc12 <42747613+llsc12@users.noreply.github.com>
2022-03-26 13:54:14 +0000

    Initial Commit

---
+++ Music Reborn/Music_RebornApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_RebornApp.swift
+//  Music Reborn
+//
+//  Created by llsc12 on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Music_RebornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
